530ab755dcb4da90200dabc7fc534ed3f60a8a92
ongsaaa <101408633+ongsaaa@users.noreply.github.com>
2024-06-26 15:56:30 +0700

    Updated parameters mentor and student models to match design

--- model/Student.model.js
+++ model/Student.model.js
@@ -1,26 +1,54 @@
 import mongoose from "mongoose";
 
 export const StudentSchema = new mongoose.Schema({
-    username : {
+    username: {
         type: String,
-        required : [true, "Please provide unique Username"],
+        required: [true, "Please provide unique Username"],
         unique: [true, "Username Exists"]
     },
     password: {
         type: String,
         required: [true, "Please provide a password"],
-        unique : false,
+        unique: false,
     },
     email: {
         type: String,
-        required : [true, "Please provide a unique email"],
+        required: [true, "Please provide a unique email"],
         unique: true,
     },
-    firstName: { type: String},
-    lastName: { type: String},
-    mobile : { type : Number},
-    address: { type: String},
-    profile: { type: String}
+    firstName: { type: String },
+    lastName: { type: String },
+    learning_pref: {
+        type: [String],
+        required: true
+    },
+    subject: {
+        type: [String], // Define subject as an array of strings
+        required: true
+    },
+    education: {
+        type: [String],
+    },
+    price_course: {
+        type: Number,
+    },
+    price_hour: {
+        type: Number,
+    },
+    line: {
+        type: String,
+        required: true
+    },
+    discord: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: Number,
+        required: true
+    },
+    address: { type: String },
+    profile: { type: String }
 });
 
 export default mongoose.model.Student || mongoose.model('Student', StudentSchema);
